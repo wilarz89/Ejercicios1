8e985c1f948cec98134dc3ff6b6c979e15ca1eac
Wilson <wjarizatruquetkd@gmail.com>
2019-07-18 23:53:06 -0500

    added code

---
+++ split.js
@@ -0,0 +1,14 @@
+// Divide cadenas segun encuentre
+
+const cadena =  "1,2,3,4,5,6,7,8,9,10,f,e,d,c,a"
+
+const resultado = cadena.split(",",2)
+
+console.log(resultado)
+// [ '1', '2' ]
+
+
+const resultado1 = cadena.split(" ")
+
+console.log(resultado1)
+// [ '1,2,3,4,5,6,7,8,9,10,f,e,d,c,a' ]
---
+++ trim.js
@@ -0,0 +1,7 @@
+// removes whitespace
+
+const letters =  " THIS IS A SENTENCE   "
+const word= letters.trim();
+
+console.log(word)
+// THIS IS A SENTENCE
